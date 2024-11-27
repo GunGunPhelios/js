091c3432060447e8629362b304cbfa374806a145
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2024-11-27 14:24:04 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const time= document.getElementById("time")
+
+document.addEventListener("DOMContentLoaded," () => { 
+    setInterval(showTime, 1000)
+})
+
+
+const showTime=() =>  (
+    let date= new Date();
+
+    let hr= date.getHours();
+    let min= date.getMinutes();
+    let sec= date.getSeconds();
+    let msec= date. getMilliseconds();
+
+
+    hr= hr <10 ? '05{hr}' : hr
+    min= min <10 ? '05{min}' : min
+    = hr <10 ? '05{hr}' : hr
+
+    console.log("hour " + hr + "minute" + min + "second" +sec)
+
+time.innerHTML= ${hr} : ${min} : ${sec}
+
+)
